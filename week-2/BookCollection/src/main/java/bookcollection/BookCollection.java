77881de34a430c3fef749390f1cfe4075c1873d9
Saranya-Ghosh <sk3757@srmist.edu.in>
2024-08-07 22:10:50 +0530

    book col

---
+++ week-2/BookCollection/src/main/java/bookcollection/BookCollection.java
@@ -0,0 +1,25 @@
+package bookcollection;
+
+import java.util.LinkedHashSet;
+
+public class BookCollection {
+    private LinkedHashSet<String> books;
+
+    public BookCollection() {
+        books = new LinkedHashSet<>();
+    }
+
+    public boolean addBook(String bookTitle) {
+        return books.add(bookTitle);
+    }
+
+    public boolean removeBook(String bookTitle) {
+        return books.remove(bookTitle);
+    }
+
+    public void displayBooks() {
+        for (String book : books) {
+            System.out.println(book);
+        }
+    }
+}
